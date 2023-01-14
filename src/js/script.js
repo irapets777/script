25b5901c7da7349a09ada6c8202807960cecff44
hase1004 <irapets777@gmail.com>
2023-01-14 14:58:09 +0200

    try

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,28 @@
-let slider = $(document).ready(function () {
-  $('.your-class').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-    dots: true,
-    infinite: true
-  });
-});
+let name = "John";
+let surname = "Doe";
+let middleName = "Patric";
 
+let arraySome = ['test', 13, true, 13, 12.56];
+const position = arraySome.indexOf(8);
+
+
+if(position>=0){
+console.log(`Число ${13} є в масиві!`);
+}else{
+  console.log(`Число ${13} не є в масиві!`)
+}
+
+
+
+
+// Array
+
+// let age = [12, 13, 22, 17];
+
+// const personalData = surname + ' ' + name[0] + '.' + ' ' + middleName[0] + '.'
+
+// console.log("Data: ", personalData)
+
+
+
+// console.log("Data: ", name);
