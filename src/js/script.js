e139412b3ceb52c3eb1a7eb6d9ff3806c61b624b
irapets777 <irapets777@gmail.com>
2023-01-15 14:59:02 +0200

    game

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,133 @@
-let name = "John";
-let surname = "Doe";
-let middleName = "Patric";
+const secNum = 7;
+const sicret = prompt("Вгадай число")
 
-let arraySome = ['test', 13, true, 13, 12.56];
-const position = arraySome.indexOf(8);
-
-
-if(position>=0){
-console.log(`Число ${13} є в масиві!`);
-}else{
-  console.log(`Число ${13} не є в масиві!`)
+if (sicret == secNum ){
+  console.log("Ви вгадали")
+} else if (sicret < secNum){
+  console.log("Введіть більше число")
+} else {
+  console.log("Введіть менше число")
 }
+
+// if (sicret == secNum){
+//   console.log("WAW")
+//   if(sicret > 7){
+   
+//   }else{
+//     console.log("ВВедіть менше число")
+//   }
+//   if(sicret < 7){
+    
+//   }else{
+//     console.log("ВВедіть більше число")
+//   }
+// }
+ 
+
+
+// let personalData = {
+//   name: "John",
+//   surName: "Patric",
+//   lastName: "Doe",
+//   addsres: {
+//     city: "Dro",
+//     street: "Rany",
+//     number: 123,
+//     name: "some",
+//     metrics:{
+//       water: 4262,
+//       gas: 738
+//     }
+//   },
+//   age: [12, 23, 45],
+//   isChildren: true
+// };
+
+// const a = prompt("Ввудіть число: ")
+
+// if (a == 0 || a > 5){
+//   console.log("Success")
+// }else{
+//   console.log("Fail")
+// }
+
+// const age = prompt("Ввеіть ваш вік: ");
+// const isActive = false;
+// console.log(typeof age);
+
+// let someArr = null;
+
+
+// if(!someArr) {
+  
+// }else{
+//   console.log("форма не працює")
+// }
+
+// if (age >=0){
+//   if (age < 18){
+//     console.log("Ви не повнолітні")
+//   }else if(age>=18 && age <21){
+//     console.log("Повнолітні в україні")
+//   }else{
+//     console.log("Повнолітні всюди")
+//   }
+//  //some logic 
+// }else{
+//  console.log("Ви ввели невірне значення");
+// }
+
+// personalData.size = size
+
+// console.log(personalData)
+
+// let metrics = personalData.addsres.metrics
+
+// console.log(metrics)
+// console.log(personalData.age[1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let name = "John";
+// let surname = "Doe";
+// let middleName = "Patric";
+
+// let arraySome = ['test', 13, true, 13, 12.56];
+// const position = arraySome.indexOf(8);
+
+
+// if(position>=0){
+// console.log(`Число ${13} є в масиві!`);
+// }else{
+//   console.log(`Число ${13} не є в масиві!`)
+// }
 
 
 
